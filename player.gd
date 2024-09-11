2ff61e350a0127c0650adc3f479653742f903567
dsangule <114325989+dsangule@users.noreply.github.com>
2024-09-11 19:24:35 +0530

    Add player animation

--- player.gd
+++ player.gd
@@ -1,18 +1,52 @@
 extends CharacterBody2D
 
-var speed = 300.0
+@onready var anim = $AnimatedSprite2D
+
+@export var speed = 300.0
+
+var isMoving = false
+var direction = "none"
 
 func _physics_process(delta):
 	velocity = Vector2()
 	
 	if Input.is_action_pressed("up"):
 		velocity.y -= speed
+		isMoving = true
+		direction = "up"
 	if Input.is_action_pressed("down"):
 		velocity.y += speed
+		isMoving = true
+		direction = "down"
 	if Input.is_action_pressed("left"):
 		velocity.x -= speed
+		isMoving = true
+		direction = "left"
 	if Input.is_action_pressed("right"):
 		velocity.x += speed
+		isMoving = true
+		direction = "right"
+	if velocity == Vector2.ZERO:
+		isMoving = false
 	
 	move_and_slide()	# doesn't work without this
-	look_at(get_global_mouse_position())	# just for sprite rotation
+	
+	if isMoving == true:
+		if direction == "up":
+			anim.play("walk_up")
+		elif direction == "down":
+			anim.play("walk_down")
+		elif direction == "left":
+			anim.play("walk_left")
+		elif direction == "right":
+			anim.play("walk_right")
+		
+	elif isMoving == false:
+		if direction == "up":
+			anim.play("idle_up")
+		elif direction == "down":
+			anim.play("idle_down")
+		elif direction == "left":
+			anim.play("idle_left")
+		elif direction == "right":
+			anim.play("idle_right")
